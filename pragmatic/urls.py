68091700be1e85c89c1236ac52429d11636e26b0
jamescode <jamescode7@gmail.com>
2022-04-06 14:35:49 +0900

    study 34 finish

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -22,5 +22,6 @@ from pragmatic import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
-    path('profile/', include('profileapp.urls')),
+    path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
