ca3bfaf00589dc2f8b39482a6c07984e8fc08e58
jamescode <jamescode7@gmail.com>
2022-04-07 02:57:10 +0900

    study 41 finish

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,8 +9,11 @@ from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 # Create your views here.
+from django.views.generic.list import MultipleObjectMixin
+
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
+from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
 
@@ -30,10 +33,16 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 
 @method_decorator(has_ownership, 'get')
--- projectapp/views.py
+++ projectapp/views.py
@@ -1,19 +1,14 @@
-from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 
-# Create your views here.
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
-from django.views.generic.edit import FormMixin
+from django.views.generic import CreateView, DetailView, ListView
 
-from articleapp.decorators import article_ownership_required
-from articleapp.forms import ArticleCreationForm
+from django.views.generic.list import MultipleObjectMixin
+
 from articleapp.models import Article
-from commentapp.forms import CommentCreationForm
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
@@ -29,10 +24,16 @@ class ProjectCreateView(CreateView):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(project=self.get_object())
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 
 class ProjectListView(ListView):
