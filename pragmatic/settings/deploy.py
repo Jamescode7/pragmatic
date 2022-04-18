6965aa454e3ae0b30218e61455da356a35ce2f90
jamescode <jamescode7@gmail.com>
2022-04-18 23:20:56 +0900

    study 60 finish

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -5,12 +5,10 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-
-
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settingspp - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
